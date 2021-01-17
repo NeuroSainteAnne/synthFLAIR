3fdf2870907b29fc1cd393aabd2e4bf752510f97
Joseph B <28443226+jbenzakoun@users.noreply.github.com>
2021-01-17 12:27:07 +0100

    Improvements and bugfixes in niftitools

--- modules/niftitools.py
+++ modules/niftitools.py
@@ -32,6 +32,19 @@ def padvolume(volume):
             volume = volume[:,cuty1:-cuty2,:]
     return volume, (padx1, padx2, pady1, pady2, cutx1, cutx2, cuty1, cuty2)
 
+def zpadding(volume, zpad):
+    orig_shape = volume.shape
+    if orig_shape[2] < zpad:
+        padz1 = int((zpad - orig_shape[2])/2)
+        padz2 = zpad - orig_shape[2] - padz1
+        volume = np.pad(volume, ((0,0),(0,0),(padz1,padz2)), mode="minimum")
+    elif orig_shape[2] > zpad:
+        cutz1 = int((orig_shape[2] - zpad)/2)
+        cutz2 = orig_shape[2] - zpad - cutz2
+        volume = volume[:,:,cutz1:cutz2]
+    return volume
+        
+
 def reversepad(volume, padspecs):
     "Reserves a previously applied padding"
     padx1 = padspecs[0]
@@ -82,6 +95,7 @@ def maskcompute(b0, b1000):
     b1000_mask, mask1000 = median_otsu(b1000, 1, 1)
     mask = binary_fill_holes(morphology.binary_dilation(brain_component(mask0 & mask1000)))
     mask = mask & (b0 >= 1) & (b1000 >= 1)
+    return mask
     
 def splitdiffusion(diffdata):
     "Splits b0 and b1000 based on value mean"
@@ -102,10 +116,45 @@ def nifti2array(diffpath):
     
     # differenciate b1000 from b0
     b0, b1000 = splitdiffusion(diffdata)
+    
+    stacked, mask, padspecs = splitdwi2array(b0,b1000,adjust=True,z_pad=False)
+    stacked = stacked.transpose([3,2,1,0])[:,:,::-1,np.newaxis,:]
+    qualarr = np.tile(2, (stacked.shape[0],1))
+    return stacked, qualarr, padspecs, diffnib.affine
+    
+def twoniftis2array(b0path, b1000path,z_pad=None):
+    # load niftis
+    diff0nib = nib.load(b0path)
+    diff0data = diff0nib.get_fdata()
+    diff1000nib = nib.load(b1000path)
+    diff1000data = diff1000nib.get_fdata()
+    return splitdwi2array(diff0data,diff1000data,adjust=False,z_pad=z_pad)
+
+def flairnifti2array(flairpath, mask, z_pad=None):
+    # load nifti
+    flairnib = nib.load(flairpath)
+    flair = flairnib.get_fdata()
+    
+    # pad
+    flair, padspecs = padvolume(flair)
+    if z_pad is not None:
+        flair = zpadding(flair, z_pad)
+        
+    # normalisation
+    flair = normalize(flair, mask)
+    
+    return flair
+
+def splitdwi2array(b0,b1000,adjust=False,z_pad=None):
         
     # pad/crop volumes to 256x256
     b0, _ = padvolume(b0)
     b1000, padspecs = padvolume(b1000)
+    
+    #Z-pad
+    if z_pad is not None:
+        b0 = zpadding(b0, z_pad)
+        b1000 = zpadding(b1000, z_pad)
     
     # ADC calculation
     adc = adccompute(b0, b1000)
@@ -118,15 +167,15 @@ def nifti2array(diffpath):
     b1000 = normalize(b1000, mask)
     
     # adjust for model input
-    b0 = ((b0 + 5) / (12 + 5))*2-1
-    b1000 = ((b1000 + 5) / (12 + 5))*2-1
-    adc = ((adc) / (7500))*2-1
+    if adjust:
+        b0 = ((b0 + 5) / (12 + 5))*2-1
+        b1000 = ((b1000 + 5) / (12 + 5))*2-1
+        adc = ((adc) / (7500))*2-1
     
     # export for model input
-    stacked = np.stack([b0,b1000,adc]).transpose([3,2,1,0])[:,:,::-1,np.newaxis,:]
-    qualarr = np.tile(2, (stacked.shape[0],1))
+    stacked = np.stack([b0,b1000,adc])
     
-    return stacked, qualarr, padspecs, diffnib.affine
+    return stacked, mask, padspecs
     
 def array2nifti(savearray, padspecs, affine, outpath):
     synthflair = savearray[:,:,::-1,0].transpose(2,1,0)
